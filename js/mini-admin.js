6a8bb71eed60eb97eb7ebeccee26c7a9cbdf6e01
adheee <adheejr77@gmail.com>
2020-03-12 21:33:03 +0800

    add img with card & img-thumbnail-color

--- js/mini-admin.js
+++ js/mini-admin.js
@@ -3,7 +3,7 @@ $(window).scroll(function () {
 
   var scroll = $(window).scrollTop();
 
-  if (scroll >= 100) {
+  if (scroll >= 70) {
     $("#navbar-top").addClass("navbar-scroll").removeClass("navbar navbar-expand-lg navbar-orange bg-hitam");
   } else {
     $("#navbar-top").removeClass("navbar-scroll").addClass("navbar navbar-expand-lg navbar-orange bg-hitam");
